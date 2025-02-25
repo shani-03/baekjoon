56ff927dac2ab1700ef96f7fde83f94a7a589583
김시환 <108675262+shani-03@users.noreply.github.com>
2025-02-25 16:36:43 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    
+    if ((num % 4 == 0 && num % 100 != 0) || (num % 400 == 0)) {
+        cout << 1 << endl;  // 윤년
+    } else {
+        cout << 0 << endl;  // 평년
+    }
+
+    return 0;
+}
